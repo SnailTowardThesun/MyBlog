460937725bb7f0672d52e15e185800b974d9276c
ME_Kun_Han <hanvskun@hotmail.com>
2017-12-09 22:55:55 +0800

    add interface in backend

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models
+from django.http import HttpResponse
+
 
 # Create your models here.
+def get_article(request):
+    return HttpResponse('1')
+
+
+def get_article_category(request):
+    return HttpResponse('2')
+
+
+def post_comment(request):
+    return HttpResponse('3')
--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
-from . import views
+from . import views, models
 
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'article', models.get_article),
+    url(r'category', models.get_article_category),
+    url(r'comment', models.post_comment)
 ]
