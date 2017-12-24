ba56fc695ebc06abd748a1ba2c913086321a0d6e
SnailTowardThesun <hanvskun@hotmail.com>
2017-12-24 13:55:21 +0800

    add about and contact part

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-import { MatButtonModule, MatCheckboxModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatGridListModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +12,8 @@ import { PlayerComponent } from './player/player.component';
 import { IndexComponent } from './index/index.component';
 import { FooterComponent } from './footer/footer.component';
 import { ArticleDetailComponent } from './article-detail/article-detail.component';
+import { AboutComponent } from './footer/about/about.component';
+import { ContactComponent } from './footer/contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +21,19 @@ import { ArticleDetailComponent } from './article-detail/article-detail.componen
     PlayerComponent,
     IndexComponent,
     FooterComponent,
-    ArticleDetailComponent
+    ArticleDetailComponent,
+    AboutComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
