ddf9bf65e7708fb2eb741fce3937412148b096b2
SnailTowardThesun <hanvskun@hotmail.com>
2018-01-05 19:33:54 +0800

    refine player page

--- frontend/src/app/player/player.component.ts
+++ frontend/src/app/player/player.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player.component.css']
 })
 export class PlayerComponent implements OnInit {
-
+  content = "coming soon ...";
   constructor() { }
 
   ngOnInit() {
