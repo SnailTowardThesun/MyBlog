4dd90d44040145708ba546f16a8d8ff6dbfe2d38
SnailTowaredThesun <hanvskun@hotmail.com>
2018-01-06 14:57:54 +0800

    bk

--- frontend/src/app/article.service.ts
+++ frontend/src/app/article.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Article, Comment, Category } from './module/article';
 
 @Injectable()
 export class ArticleService {
 
   constructor() { }
+
+  getAritcleByPage(page: number): Article[] {
+    var arr: Article[] = [
+    ];
+
+    return arr;
+  }
+
+  getArticlePages(): number {
+    return 0;
+  }
+
+  getArticleComments(article: Article): Comment[] {
+    var comments: Comment[] = [
+
+    ];
+
+    return comments;
+  }
+
+  getTopCategoeries(top: number): Category[] {
+    var categories: Category[] = [
+
+    ];
+
+    return categories;
+  }
 
 }
